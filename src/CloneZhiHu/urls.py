5d233b8202e6388f497267de0f553b03327a2d09
sonia <844374782@qq.com>
2017-03-13 10:50:59 +0800

    update

--- src/CloneZhiHu/urls.py
+++ src/CloneZhiHu/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from zhihuApp import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/', views.home, name= 'home'),
 ]
